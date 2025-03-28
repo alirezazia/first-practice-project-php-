921a81ea881747b571b92c157141bc0f04220fcf
seyed alireza ziaei <arziayee80@gmail.com>
2025-03-28 15:06:08 +0330

    Styles link modified

--- panel/layouts/sidebar.php
+++ panel/layouts/sidebar.php
@@ -1,5 +1,7 @@
+<?php
+ ?>
 <link rel="stylesheet" href="../../assets/css/bootstrap.min.css" media="all" type="text/css">
-    <link rel="stylesheet" href="../../assets/css/style.css" media="all" type="text/css">
+<link rel="stylesheet" href="../../assets/css/style.css" media="all" type="text/css">
 <section class="sidebar">
     <section class="sidebar-link">
         <a href="">panel</a>
