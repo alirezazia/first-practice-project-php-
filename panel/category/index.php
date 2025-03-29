f223e0abdf1d057747284ff8280a02a993a5f6f4
seyed alireza ziaei <arziayee80@gmail.com>
2025-03-29 05:03:44 +0330

    Put the link to create a category

--- panel/category/index.php
+++ panel/category/index.php
@@ -27,7 +27,7 @@ require_once '../../functions/pdo_connection.php';
 
                     <section class="mb-2 d-flex justify-content-between align-items-center">
                         <h2 class="h4">Categories</h2>
-                        <a href="create.php" class="btn btn-sm btn-success">Create</a>
+                        <a href="<?= url('/panel/category/create.php') ?>" class="btn btn-sm btn-success">Create</a>
                     </section>
 
                     <section class="table-responsive">
@@ -53,7 +53,7 @@ require_once '../../functions/pdo_connection.php';
                                     <td><?= $category->name ?></td>
                                     <td>
                                         <a href="" class="btn btn-info btn-sm">Edit</a>
-                                        <a href="" class="btn btn-danger btn-sm">Delete</a>
+                                        <a href="<?= url('panel/category/delete.php') . '?cat_id=' . $category->id ?>" class="btn btn-danger btn-sm">Delete</a>
                                     </td>
                                 </tr>
                                 <?php } ?>
